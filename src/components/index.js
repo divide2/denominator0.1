e796df5d3913bde3cc6f7f84c572feffda12194a
ctt123 <283569035@qq.com>
2019-02-14 17:15:07 +0800

    remov js

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-import Page from './Page.vue';
-Vue.component('Page', Page);
--- src/route/Token.js
+++
@@ -1,9 +0,0 @@
-export function getToken() {
-    return localStorage.getItem('token');
-}
-export function setToken(token) {
-    localStorage.setItem("token", token);
-}
-export function clearToken() {
-    localStorage.removeItem("token");
-}
